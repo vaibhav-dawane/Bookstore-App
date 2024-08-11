9072eaa90cda31346e43cacb5d7896a2aa231e4f
vaibhav-dawane <vaibhavdawane07@gmail.com>
2024-08-11 14:04:37 +0530

    added vercel.json

--- Backend/index.js
+++ Backend/index.js
@@ -8,14 +8,20 @@ import cors from "cors"
 const app = express()
 dotenv.config()
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/bookstore")
     .then(() => {
         console.log("MongoDB Connected"); 
     })
